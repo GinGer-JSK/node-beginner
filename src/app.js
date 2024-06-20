02c90c1d6d3194f95b647b132ff4f55834ca64a3
GinGer-JSK <jskim5701@gmail.com>
2024-06-20 21:20:05 +0900

    feat:에러 처리 미들웨어 구현

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from './constants/env.constant.js';
 import { productRouter } from './routers/products.router.js';
 import { connect } from './schemas/index.js';
+import { errorHandler } from './middlewares/error-handler.middleware.js';
 const app = express();
 
 connect();
@@ -9,6 +10,7 @@ connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', productRouter);
+app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
   console.log(`서버오픈 ${SERVER_PORT} `);
